953245f1e3b1f60ba2979c511ac17b2194c0d0c5
YBkhrv <33148793+YBkhrv@users.noreply.github.com>
2021-02-01 11:34:34 +0600

    SearchEngine
    "Альтернативный проект на C#-3. Программа для поиска файлов по заданным критериям"

---
+++ Search engine/Search/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Search
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 61);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Leave += new System.EventHandler(this.TextBox_Leave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(16, 134);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(212, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Leave += new System.EventHandler(this.TextBox_Leave);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(19, 203);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(212, 22);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Leave += new System.EventHandler(this.TextBox_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Стартовая директория";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 103);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Шаблон имени файла";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 182);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(210, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Текст, содержащийся в файле";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(562, 55);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Начать поиск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(538, 131);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 28);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Остановить поиск";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(538, 203);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 28);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Продолжить поиск";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(163, 331);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Результат:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(152, 377);
+            this.labelResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(105, 17);
+            this.labelResult.TabIndex = 9;
+            this.labelResult.Text = "Имена файлов";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(778, 465);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Search engine";
+            this.Text = "Search engine";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ Search engine/Search/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+//"Альтернативный проект на C#-3. Программа для поиска файлов по заданным критериям"
+//Критерии:
+//а) Стартовая директория(с которой начинается поиск)
+//б) Шаблон имени файла
+//в) Текст, содержащийся в файле
+//е) Введенные критерии не должны потеряться при перезапуске программы
+//-г) Во время поиска нужно отображать какой файл обрабатывается в данный момент, количество обработанных файлов и прошедшее время.
+//-д) Все найденные файлы отображать в виде дерева (как в левой части проводника)
+//-е) Найденные файлы должны обновляться в реальном времени
+//ж) Поиск нужно уметь остановить в любой момент и затем либо продолжить, либо начать новый
+//Бахарева Ю
+
+
+
+namespace Search
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            LoadValues();
+            ResultText = "";
+        }
+
+        public string StartingDirectory { get => textBox1.Text; set => textBox1.Text = value; }
+        public string PatternNameFiles { get => textBox2.Text; set => textBox2.Text = value; }
+        public string SearchText { get => textBox3.Text; set => textBox3.Text = value; }
+        public string ResultText { get => labelResult.Text; set => labelResult.Text = value; }
+        private bool isStopSearch = false;
+
+        private void SaveValue()
+        {
+            using (BinaryWriter file = new BinaryWriter(File.Create("values.bin")))
+            {
+                file.Write(StartingDirectory);
+                file.Write(PatternNameFiles);
+                file.Write(SearchText);
+            }
+        }
+
+        private void LoadValues()
+        {
+            try
+            {
+                using (BinaryReader file = new BinaryReader(File.OpenRead("values.bin")))
+                {
+                    string _startingDirectory = file.ReadString();
+                    string _patternNameFiles = file.ReadString();
+                    string _searchText = file.ReadString();
+                    StartingDirectory = _startingDirectory;
+                    PatternNameFiles = _patternNameFiles;
+                    SearchText = _searchText;
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+
+
+        public IEnumerable<string> SearchTextInDirectory(string directory, string patternNameFiles, string searchText)
+        {
+            string[] namesFiles = Directory.GetFiles(directory, patternNameFiles);
+            foreach (string name in namesFiles)
+            {
+                if (isStopSearch)
+                    break;
+                if (File.ReadAllText(name).Contains(searchText))
+                    yield return name;
+            }
+        }
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            isStopSearch = false;
+            ResultText = "";
+            foreach (string name in SearchTextInDirectory(StartingDirectory, PatternNameFiles, SearchText))
+            {
+                ResultText += name + Environment.NewLine;
+            }
+        }
+
+        private void TextBox_Leave(object sender, EventArgs e)
+            => SaveValue();
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            isStopSearch = true;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
